256c36cfc640fd64180369d0cd3f1eb3d197c138
guojy1314 <827447989@qq.com>
2019-05-21 10:39:45 +0800

    收藏点赞ok
    利用本地缓存收藏点赞ok，多个页面登录ok

--- miniprogram/pages/answer_detail/answer_detail.js
+++ miniprogram/pages/answer_detail/answer_detail.js
@@ -1,4 +1,5 @@
 //answer.js
+var app = getApp()
 var common = require('../../utils/common.js')
 const db = wx.cloud.database()
 const ans = db.collection('share_answer')
@@ -11,10 +12,13 @@ Page({
    */
   data: {
     queList: {},
-    ansList:{},
+    ansList: {},
     num: 0,
-    isVote:0,
-    isCollect:0
+    isVote: false,
+    isCollect: false,
+    hasUserInfo: false,
+    userInfo: {},
+    userOpenID: ''
   },
 
   /**
@@ -42,25 +46,49 @@ Page({
         })
       }
     })
-    
+    // 读取所有的文章列表点赞缓存状态
+    var cache = wx.getStorageSync('voted');
+    // 如果缓存状态存在
+    if (cache) {
+      // 拿到所有缓存状态中的1个
+      var currentCache = cache[id];
+      // 把拿到的缓存状态中的1个赋值给data中的collection，如果当前文章没有缓存状态，currentCache 的值就是 false，如果当前文章的缓存存在，那么 currentCache 就是有值的，有值的说明 currentCache 的值是 true
+      if (currentCache) {
+        this.setData({
+          isVote: true
+        })
+      }
+    } else {
+      // 如果所有的缓存状态都不存在 就让不存在的缓存存在
+      var voted_cache = {};
+      // 既然所有的缓存都不存在，那么当前这个文章点赞的缓存也不存在，我们可以把当前这个文章点赞的缓存值设置为 false
+      wx.setStorageSync('voted', voted_cache);
+
+    }
+
+
+    // 读取所有的文章列表收藏缓存状态
+    var collected_cache = wx.getStorageSync('collected');
+    // 如果缓存状态存在
+    if (collected_cache) {
+      // 拿到所有缓存状态中的1个
+      var col_currentCache = collected_cache[id];
+      // 把拿到的缓存状态中的1个赋值给data中的collection，如果当前文章没有缓存状态，currentCache 的值就是 false，如果当前文章的缓存存在，那么 currentCache 就是有值的，有值的说明 currentCache 的值是 true
+      if (col_currentCache) {
+        this.setData({
+          isCollect: true
+        })
+      }
+
+    } else {
+      // 如果所有的缓存状态都不存在 就让不存在的缓存存在
+      var collected_cache = {};
+      // 既然所有的缓存都不存在，那么当前这个文章点赞的缓存也不存在，我们可以把当前这个文章点赞的缓存值设置为 false
+      wx.setStorageSync('collected', collected_cache);
+    }
+
   },
 
-  //添加到收藏夹
-  addFavorites: function(options) {
-    let article = this.data.article; //获取当前新闻
-    wx.setStorageSync(article.question_id, article); //添加到本地缓存
-    this.setData({
-      isAdd: true
-    }); //更新按钮显示
-  },
-  //取消收藏
-  cancelFavorites: function() {
-    let article = this.data.article; //获取当前新闻
-    wx.removeStorageSync(article.question_id); //从本地缓存删除
-    this.setData({
-      isAdd: false
-    }); //更新按钮显示
-  },
 
   /**
    * 自定义函数-跳转到问题详情
@@ -69,7 +97,7 @@ Page({
     common.goToQuestion(e.currentTarget.dataset.id)
   },
 
-  vote:function(e){
+  vote: function(e) {
     wx.cloud.callFunction({
       name: 'vote',
       data: {
@@ -77,26 +105,107 @@ Page({
       },
       success: res => {
         this.setData({
-          isVote:1
+          isVote: true
         })
+        var cache = wx.getStorageSync('voted');
+        cache[this.data.ansList[0]._id] = true;
+        wx.setStorageSync('voted', cache);
+
         console.log('更新数据成功')
       }
     })
   },
 
-  /**
-   * 生命周期函数--监听页面初次渲染完成
-   */
-  onReady: function() {
+  minusVote: function(e) {
+    wx.cloud.callFunction({
+      name: 'minusVote',
+      data: {
+        _id: this.data.ansList[0]._id
+      },
+      success: res => {
+        this.setData({
+          isVote: false
+        })
+        var cache = wx.getStorageSync('voted');
+        cache[this.data.ansList[0]._id] = false;
+        wx.setStorageSync('voted', cache);
+        console.log('更新数据成功')
+      }
+    })
+  },
 
+  collect: function(e) {
+    var cache = wx.getStorageSync('collected');
+    cache[this.data.ansList[0]._id] = true;
+    wx.setStorageSync('collected', cache);
+    this.setData({
+      isCollect: true
+    })
+    console.log('收藏成功')
+  },
+
+  cancleCollect: function(e) {
+    var cache = wx.getStorageSync('collected');
+    cache[this.data.ansList[0]._id] = false;
+    wx.setStorageSync('collected', cache);
+    this.setData({
+      isCollect: false
+    })
+    console.log('取消收藏成功')
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function() {
-
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        userOpenID: app.globalData.openid,
+        hasUserInfo: true
+      })
+    } else if (this.data.canIUse) {    // 由于 getUserInfo 是网络请求，可能会在 Page.onLoad 之后才返回
+      // 所以此处加入 callback 以防止这种情况
+      app.userInfoReadyCallback = res => {
+        this.setData({
+          userInfo: res.userInfo,
+          userOpenID: app.globalData.openid,
+          hasUserInfo: true
+        })
+      }
+    }
   },
+
+  // 获取用户信息
+  getUserInfo: function(e) {
+    console.log(e)
+    app.globalData.userInfo = e.detail.userInfo
+    this.setData({
+      userInfo: e.detail.userInfo,
+      hasUserInfo: true
+    })
+  },
+
+  onGetOpenid: function() {
+    // 调用云函数
+    wx.cloud.callFunction({
+      name: 'login',
+      data: {},
+      success: res => {
+        console.log('[云函数] [login] user openid: ', res.result.openid)
+        app.globalData.openid = res.result.openid
+        this.setData({
+          userOpenID: res.result.openid
+        })
+
+      },
+      fail: err => {
+        console.error('[云函数] [login] 调用失败', err)
+
+      }
+    })
+  },
+
 
   /**
    * 生命周期函数--监听页面隐藏
